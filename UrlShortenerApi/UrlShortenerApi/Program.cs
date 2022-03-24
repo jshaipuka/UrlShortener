c1d22b129d3e7d6dfa186c2e7457230c183d3ff1
jshaipuka <jelena.shaipuka@gmail.com>
2022-03-24 19:07:25 +0200

    Adding InMemoryDB

--- UrlShortenerApi/UrlShortenerApi/Program.cs
+++ UrlShortenerApi/UrlShortenerApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using UrlShortenerApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,20 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ShortUrlDb>(options => {
+    options.UseInMemoryDatabase(databaseName: "MyLocalDb"); 
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app
-    .MapGet("/url/{urlHash}", (string urlHash) =>
-    {
-        var url = new ShortUrl() { UrlHash = urlHash, OriginalUrl = "https://google.com" };
-        
-        if (url == null) return Results.NotFound();
-        
-        return Results.Ok(url.OriginalUrl);
-    })
+    .MapGet("/url/{urlHash}", async (ShortUrlDb db, string urlHash) =>
+        await db.ShortUrls.SingleOrDefaultAsync(su => su.UrlHash == urlHash) is ShortUrl url
+        ? Results.Ok(url)
+        : Results.NotFound())
     .Produces<string>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound)
     .WithName("GetOriginalUrl")

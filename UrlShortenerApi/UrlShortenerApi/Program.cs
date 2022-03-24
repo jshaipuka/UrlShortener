b5625b0c923d5ee43652358924111becd679f58e
jshaipuka <jelena.shaipuka@gmail.com>
2022-03-24 18:30:59 +0200

    Add swaggeer

--- UrlShortenerApi/UrlShortenerApi/Program.cs
+++ UrlShortenerApi/UrlShortenerApi/Program.cs
@@ -1,12 +1,23 @@
 using UrlShortenerApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/url/{urlHash}", (string urlHash) =>
-{
-    var url = new ShortUrl() { UrlHash = urlHash, OriginalUrl = "https://google.com" };
-    return url.OriginalUrl;
-});
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app
+    .MapGet("/url/{urlHash}", (string urlHash) =>
+    {
+        var url = new ShortUrl() { UrlHash = urlHash, OriginalUrl = "https://google.com" };
+        return url.OriginalUrl;
+    })
+    .Produces<string>(StatusCodes.Status200OK)
+    .WithName("GetOriginalUrl")
+    .WithTags("Getters");
 
 app.Run();

e17dd071608491e7d8df97bc42c9192dc4dcc464
jshaipuka <jelena.shaipuka@gmail.com>
2022-03-24 18:22:48 +0200

    Adding model for ShortUrl

---
+++ UrlShortenerApi/UrlShortenerApi/Models/ShortUrl.cs
@@ -0,0 +1,8 @@
+namespace UrlShortenerApi.Models;
+
+public class ShortUrl
+{
+    public string UrlHash { get; set; }
+
+    public string OriginalUrl { get; set; }
+}
--- UrlShortenerApi/UrlShortenerApi/Program.cs
+++ UrlShortenerApi/UrlShortenerApi/Program.cs
@@ -1,6 +1,12 @@
+using UrlShortenerApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/url/{urlHash}",  () => "Return original URL");
+app.MapGet("/url/{urlHash}", (string urlHash) =>
+{
+    var url = new ShortUrl() { UrlHash = urlHash, OriginalUrl = "https://google.com" };
+    return url.OriginalUrl;
+});
 
 app.Run();
